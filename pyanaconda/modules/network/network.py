2db496cdde04da85270304baf7a53b1af098e9f4
Radek Vykydal <rvykydal@redhat.com>
2018-02-05 11:14:01 +0100

    network module: connect DeviceConfigurations to signals from NM

--- pyanaconda/modules/network/network.py
+++ pyanaconda/modules/network/network.py
@@ -158,5 +158,10 @@ class NetworkModule(KickstartModule):
         # TODO use get_all_devices?, virtual configs? test this
         # TODO: idempotent?
         self._device_configurations = DeviceConfigurations(self.nm_client)
+        self._device_configurations.configuration_changed.connect(self.device_configurations_changed_cb)
         self._device_configurations.reload()
+        self._device_configurations.connect()
+
+    def device_configurations_changed_cb(self, old_dev_cfg, new_dev_cfg):
+        log.debug("Configuration changed: %s -> %s", old_dev_cfg, new_dev_cfg)
         log.debug("%s", self._device_configurations)
