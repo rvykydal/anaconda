bb034e6ca8b12572fe6c25c92ab97bb2b4a601d7
Radek Vykydal <rvykydal@redhat.com>
2018-02-16 14:25:13 +0100

    network module: provide apply_kickstart

--- pyanaconda/modules/network/kickstart.py
+++ pyanaconda/modules/network/kickstart.py
@@ -18,6 +18,7 @@
 # Red Hat, Inc.
 #
 from pyanaconda.core.kickstart import VERSION, KickstartSpecification, commands as COMMANDS
+from pykickstart.constants import BIND_TO_MAC
 
 DEFAULT_DEVICE_SPECIFICATION = "link"
 
@@ -64,6 +65,15 @@ def update_network_data_with_default_device(network_data_list, device_specificat
             nd.device = device_specification
             updated = True
     return updated
+
+def update_first_network_command_activate_value(network_data_list):
+    if network_data_list:
+        nd = network_data_list[0]
+        if not is_hostname_only_network_data(nd):
+            if nd.activate is None:
+                nd.activate = True
+                return True
+    return False
 
 def is_hostname_only_network_args(args):
     return (len(args) == 1 and args[0].startswith("--hostname") or
